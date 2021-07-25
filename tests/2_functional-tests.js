2e734e821b6486319a18513589e5b0e2df64747a
lChap701 <chapmanlucas201@gmail.com>
2021-07-25 14:37:17 -0500

    Added the issue module

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,6 +2,7 @@ const chaiHttp = require("chai-http");
 const chai = require("chai");
 const assert = chai.assert;
 const server = require("../server");
+const Issue = require("../issue");
 const crud = require("../crud");
 
 chai.use(chaiHttp);
@@ -230,151 +231,183 @@ suite("Functional Tests", function () {
     });
   });
 
-  // suite("PUT /api/issues/ Tests", () => {
-  //   test("1)  One Field Test", () => {
-  //     const PROJECT = "test";
-  //     crud.getProject(PROJECT).then((project) => {
-  //       crud.getIssue({ project: project._Id }).then(
-  //         (issue) => {
-  //           console.log(issue);
-  //           const ID = issue._id;
-  //           const data = {
-  //             _id: ID,
-  //             issue_text: "Testing updates on one field",
-  //           };
+  suite("PUT /api/issues/ Tests", () => {
+    test("1)  One Field Test", () => {
+      const exIssue = new Issue(
+        "Test 3",
+        "Testing updates on one field",
+        "Lucas Chapman"
+      );
 
-  //           chai
-  //             .request(server)
-  //             .put(PATH + "test")
-  //             .send(data)
-  //             .end((err, res) => {
-  //               if (err) {
-  //                 console.log(err);
-  //               } else {
-  //                 assert.deepPropertyVal(
-  //                   JSON.parse(res.text),
-  //                   "result",
-  //                   "successfully updated",
-  //                   `'${res.text}' should have a property of 'result' that is equal to 'successfully updated'`
-  //                 );
-  //               }
-  //             });
-  //         }
-  //       );
-  //     });
-  //   });
+      crud.getProject("test").then((project) => {
+        const PROJECT = project._id;
+        exIssue.project = PROJECT;
 
-  // test("2)  Multiple Fields Test", () => {
-  //   crud.getIssue({ issue_title: "Test 2" }).then(
-  //     (issue) => {
-  //       const ID = issue._id;
-  //       const data = {
-  //         _id: ID,
-  //         issue_text: "Testing updates on multiple fields",
-  //         assigned_to: "John Smith",
-  //         status_text: "Open"
-  //       };
+        crud.addIssue(exIssue).then((issue) => {
+          const ID = issue._id;
+          const data = {
+            _id: ID,
+            status_text: "Open",
+          };
 
-  //       chai
-  //         .request(server)
-  //         .put(PATH + "testing")
-  //         .send(data)
-  //         .end((err, res) => {
-  //           if (err) {
-  //             console.log(err);
-  //           } else {
-  //             assert.deepPropertyVal(
-  //               JSON.parse(res.text),
-  //               "result",
-  //               "successfully updated",
-  //               `'${res.text}' should have a property of 'result' that is equal to 'successfully updated'`
-  //             );
-  //           }
-  //         });
-  //     }
-  //   );
-  // });
+          project.issues.push(issue);
+          project.save();
 
-  // test("3)  No Fields Test", () => {
-  //     crud.getIssue({ issue_title: "Test 2" }).then(
-  //       (issue) => {
-  //         const data = {
-  //           _id: issue._id
-  //         };
+          chai
+            .request(server)
+            .put(PATH + "test")
+            .send(data)
+            .end((err, res) => {
+              if (err) {
+                console.log(err);
+              } else {
+                assert.deepPropertyVal(
+                  JSON.parse(res.text),
+                  "result",
+                  "successfully updated",
+                  `'${res.text}' should have a property of 'result' that is equal to 'successfully updated'`
+                );
+              }
+            });
+        });
+      });
+    });
 
-  //         chai
-  //           .request(server)
-  //           .put(PATH + "testing")
-  //           .send(data)
-  //           .end((err, res) => {
-  //             if (err) {
-  //               console.log(err);
-  //             } else {
-  //               assert.deepPropertyVal(
-  //                 JSON.parse(res.text),
-  //                 "error",
-  //                 "no update field(s) sent",
-  //                 `'${res.text}' should have a property of 'error' that is equal to 'no update field(s) sent'`
-  //               );
-  //             }
-  //           });
-  //       }
-  //     );
-  // });
+    test("2)  Multiple Fields Test", () => {
+      const exIssue = new Issue(
+        "Test 4",
+        "Testing updates on multiple fields",
+        "Lucas Chapman"
+      );
 
-  //   test("4)  No _id Test", () => {
-  //     const data = {
-  //       issue_text: "Testing updates without an _id",
-  //       assigned_to: "John Smith",
-  //       status_text: "Closed",
-  //       open: false,
-  //     };
+      crud.getProject("testing").then((project) => {
+        const PROJECT = project._id;
+        exIssue.project = PROJECT;
 
-  //     chai
-  //       .request(server)
-  //       .put(PATH + "testing")
-  //       .send(data)
-  //       .end((err, res) => {
-  //         if (err) {
-  //           console.log(err);
-  //         } else {
-  //           assert.deepPropertyVal(
-  //             JSON.parse(res.text),
-  //             "error",
-  //             "missing _id",
-  //             `'${res.text}' should have a property of 'error' that is equal to 'missing _id'`
-  //           );
-  //         }
-  //       });
-  //   });
+        crud.addIssue(exIssue).then((issue) => {
+          const ID = issue._id;
+          const data = {
+            _id: ID,
+            assigned_to: "John Smith",
+            status_text: "Close",
+            open: false,
+          };
 
-  //   test("5)  Invalid _id Test", () => {
-  //     const data = {
-  //       _id: "7597e280d35ae174eeddf13c",
-  //       issue_text: "Testing updates with an invalid _id",
-  //       assigned_to: "John Smith",
-  //       status_text: "Closed",
-  //       open: false,
-  //     };
+          project.issues.push(issue);
+          project.save();
 
-  //     chai
-  //       .request(server)
-  //       .put(PATH + "test")
-  //       .send(data)
-  //       .end((err, res) => {
-  //         if (err) {
-  //           console.log(err);
-  //         } else {
-  //           assert.deepPropertyVal(
-  //             JSON.parse(res.text),
-  //             "error",
-  //             "could not update",
-  //             `'${res.text}' should have a property of 'error' that is equal to 'could not update'`
-  //           );
-  //         }
-  //       });
-  //   });
-  // });
+          chai
+            .request(server)
+            .put(PATH + "testing")
+            .send(data)
+            .end((err, res) => {
+              if (err) {
+                console.log(err);
+              } else {
+                assert.deepPropertyVal(
+                  JSON.parse(res.text),
+                  "result",
+                  "successfully updated",
+                  `'${res.text}' should have a property of 'result' that is equal to 'successfully updated'`
+                );
+              }
+            });
+        });
+      });
+    });
+
+    test("3)  No Fields Test", () => {
+      const exIssue = new Issue(
+        "Test 5",
+        "Testing updates with no fields",
+        "Lucas Chapman"
+      );
+
+      crud.getProject("fail").then((project) => {
+        const PROJECT = project._id;
+        exIssue.project = PROJECT;
+
+        crud.addIssue(exIssue).then((issue) => {
+          const ID = issue._id;
+          const data = { _id: ID };
+
+          project.issues.push(issue);
+          project.save();
+
+          chai
+            .request(server)
+            .put(PATH + "fail")
+            .send(data)
+            .end((err, res) => {
+              if (err) {
+                console.log(err);
+              } else {
+                assert.deepPropertyVal(
+                  JSON.parse(res.text),
+                  "error",
+                  "no update field(s) sent",
+                  `'${res.text}' should have a property of 'error' that is equal to 'no update field(s) sent'`
+                );
+              }
+            });
+        });
+      });
+    });
+
+    test("4)  No _id Test", () => {
+      const data = new Issue(
+        "Test 6",
+        "Testing updates with no _id",
+        "John Smith",
+        "Closed",
+        false
+      );
+
+      chai
+        .request(server)
+        .put(PATH + "fail")
+        .send(data)
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+          } else {
+            assert.deepPropertyVal(
+              JSON.parse(res.text),
+              "error",
+              "missing _id",
+              `'${res.text}' should have a property of 'error' that is equal to 'missing _id'`
+            );
+          }
+        });
+    });
+
+    test("5)  Invalid _id Test", () => {
+      const data = {
+        _id: "7597e280d35ae174eeddf13c",
+        issue_text: "Testing updates with an invalid _id",
+        assigned_to: "John Smith",
+        status_text: "Closed",
+        open: false,
+      };
+
+      chai
+        .request(server)
+        .put(PATH + "fail")
+        .send(data)
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+          } else {
+            assert.deepPropertyVal(
+              JSON.parse(res.text),
+              "error",
+              "could not update",
+              `'${res.text}' should have a property of 'error' that is equal to 'could not update'`
+            );
+          }
+        });
+    });
+  });
 
   // suite("DELETE /api/issues/ Tests", () => {
   //   test("1)  Delete Issue Test", () => {
